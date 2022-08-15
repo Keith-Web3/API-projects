3a1ecd38980800a32f5f338c3fb920612ec665f7
Ola <olorunnisholaolamilekan@gmail.com>
2022-08-15 18:10:13 +0100

    Added extra map features

--- ip-address-tracker-master/index.js
+++ ip-address-tracker-master/index.js
@@ -3,7 +3,27 @@ const searchInput = document.getElementById("search-input")
 const modalInfo = document.querySelectorAll(".info > p")
 const fullScreenIcon = document.getElementById("compress-icon")
 const mapDiv = document.getElementById("map")
-let map
+
+let map = L.map('map').fitWorld();
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+map.locate({setView: true, maxZoom: 16});
+
+map.addEventListener('locationfound', function(e) {
+  var radius = e.accuracy;
+
+  L.marker(e.latlng).addTo(map)
+      .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+  L.circle(e.latlng, radius).addTo(map);
+});
+
+map.addEventListener('locationerror', function(e) {
+  alert(e.message);
+});
 
 async function* getMap() {
   try {
