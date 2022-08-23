3e339614831765d109489ac3d89be9cced0c5d93
Ola <olorunnisholaolamilekan@gmail.com>
2022-08-23 14:31:29 +0100

    added dark mode

--- rest-countries-api-master/index.js
+++ rest-countries-api-master/index.js
@@ -45,4 +45,7 @@ searchInput.previousElementSibling.addEventListener("click", submitSearchInput)
 
 colorMode.addEventListener('click', function() {
   document.body.classList.toggle("dark-mode")
+  document.body.classList.contains("dark-mode") ? 
+    (colorMode.children[0].src = "./images/moon-solid.svg", selectFilter.children[1].src = "./images/icon-arrow-light.svg") : 
+    (colorMode.children[0].src = "./images/icon-moon.svg", selectFilter.children[1].src = "./images/icon-arrow.svg")
 })
